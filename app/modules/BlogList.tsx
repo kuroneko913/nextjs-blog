87ad64516499c3dc9c46878816686d3dedabd1bf
kuroneko913 <myblackcat7112+github@gmail.com>
2024-09-24 08:38:03 +0900

    [fix] 見た目の調整

--- app/modules/BlogList.tsx
+++ app/modules/BlogList.tsx
@@ -3,8 +3,8 @@ import ArticleBox from "./ArticleBox";
 
 export default function BlogList(prop: { posts: Post[] }) {
     return (
-        <div className="p-10 sm:w-3/4 w-full">
-            <h1 className="text-2xl pb-10 font-bold">About</h1>
+        <div className="sm:w-3/4 w-full">
+            <h1 className="text-2xl pb-10 font-bold">Blog</h1>
             <div className="flex gap-8 flex-wrap justify-start items-start">
                 {prop.posts.map((post) => (
                     <ArticleBox post={post} key={post.slug} />
--- app/modules/Header.tsx
+++ app/modules/Header.tsx
@@ -33,21 +33,21 @@ export default function Header() {
                     </div>
                 </div>
                 <ul className="space-y-8 text-xl font-bold text-gray-900 px-6 w-full">
-                   <li><a className="hover:text-blue-500" href="/">Home</a></li>
-                   <li><a className="hover:text-blue-500" href="/blog">Blog</a></li>
-                   <li><a className="hover:text-blue-500" href="/about">About</a></li>
+                   <li><a className="block hover:text-gray-500 py-2 px-4 w-full" href="/">Home</a></li>
+                   <li><a className="block hover:text-gray-500 py-2 px-4 w-full" href="/blog">Blog</a></li>
+                   <li><a className="block hover:text-gray-500 py-2 px-4 w-full" href="/about">About</a></li>
                </ul>
             </div>
             {/* 通常のメニュー: sm以上で表示 */}
             <ul className="hidden sm:flex sm:space-x-16 text-gray-900 font-bold items-center">
                 <li>
-                    <a className="hover:text-blue-500" href="/">Home</a>
+                    <a className="hover:text-gray-500" href="/">Home</a>
                 </li>
                 <li>
-                    <a className="hover:text-blue-500" href="/blog">Blog</a>
+                    <a className="hover:text-gray-500" href="/blog">Blog</a>
                 </li>
                 <li>
-                    <a className="hover:text-blue-500" href="/about">About</a>
+                    <a className="hover:text-gray-500" href="/about">About</a>
                 </li>
             </ul>
         </header>
