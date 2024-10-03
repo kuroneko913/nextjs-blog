d8139272ba1e359eda074dbed56a2d0fb6e44ed4
kuroneko913 <myblackcat7112+github@gmail.com>
2024-10-04 01:47:33 +0900

    [fix] スマホからだとTwitterTimeLineが表示されないらしいので、スマホだったら何もしないように変更

--- app/modules/TwitterTimeLine.tsx
+++ app/modules/TwitterTimeLine.tsx
@@ -1,15 +1,29 @@
 'use client';
 
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export default function TwitterTimeLine() {
-    useEffect(() => {
+    const [isMobile, setIsMobile] = useState(false);
+    useEffect(()=>{
+        const userAgent = window.navigator.userAgent.toLowerCase();
+        const isMobile = /iphone|ipod|ipad|android/.test( userAgent );
+        setIsMobile(isMobile);
+    }, []);
+    useEffect(()=>{
+        if (isMobile) {
+            return;
+        }
         const script = document.createElement("script");
         script.src = "https://platform.twitter.com/widgets.js";
         script.async = true;
         document.body.appendChild(script);
-    }, []);
-
+    }, [isMobile]);
+    // Twitterタイムラインがスマホからだと表示されないようなので、スマホの場合は何も表示しない。
+    if (isMobile) {
+        return (
+            <div></div>
+        );
+    }
     return (
         <div className="mt-10 max-w-sm mx-auto h-[50vh] sm:h-[100vh] overflow-scroll">
             <a className="twitter-timeline" 
