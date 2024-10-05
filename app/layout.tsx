f581be6ad08381490c386d05a69d70aba035ce51
kuroneko913 <myblackcat7112+github@gmail.com>
2024-10-05 15:22:13 +0900

    [fix] サイト全体のSEO対策でOGPをセット

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,19 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "くろねこ。の実験室",
   description: "自由気ままに書くブログサイト",
+  openGraph: {
+    type: "website",
+    locale: "ja_JP",
+    title: "くろねこ。の実験室",
+    description: "自由気ままに書くブログサイト",
+    siteName: "くろねこ。の実験室",
+    url: "https://myblackcat913.com",
+    images: [
+      {
+        url: "https://myblackcat913.com/images/logo.webp",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
