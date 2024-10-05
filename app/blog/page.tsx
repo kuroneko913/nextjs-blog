14a22c31968592019bba2437e4ede8bbaac071be
kuroneko913 <myblackcat7112+github@gmail.com>
2024-10-05 15:28:28 +0900

    [fix] ブログページのOGPなどを設定

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -4,6 +4,25 @@ import Hero from "@/app/modules/Hero";
 import Blog from "@/app/modules/Blog";
 import Footer from "@/app/modules/Footer";
 import { SearchParams } from "@/src/interfaces/post";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+    title: "ブログ - くろねこ。の実験室",
+    description: "自由気ままに書くブログサイト",
+    openGraph: {
+      type: "website",
+      locale: "ja_JP",
+      title: "ブログ - くろねこ。の実験室",
+      description: "自由気ままに書くブログ",
+      siteName: "くろねこ。の実験室",
+      url: "https://myblackcat913.com/blog",
+      images: [
+        {
+          url: "https://myblackcat913.com/images/logo.webp",
+        },
+      ],
+    },
+  };
 
 export default async function Index(props: { searchParams: SearchParams }) {
     // 記事を取得する。
