55fadc577f01c6d2bf09e3ca053241f489a4c5fd
kuroneko913 <myblackcat7112+github@gmail.com>
2025-05-11 03:38:34 +0900

    [delete] 不要になったファイルを削除

--- app/api/labs/mcp-tools/get_weather/route.tsx
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from 'next/server';
-import { get_weather } from './logic';
-
-export async function POST(req: Request): Promise<Response> {
-    const reqBody = await req.json();
-    const { arguments: { location } } = reqBody;
-    const result = await get_weather({ location });
-    return NextResponse.json(result);
-}
--- app/api/labs/mcp-tools/now/route.tsx
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from 'next/server';
-import { now } from './logic';
-
-export async function POST(req: Request): Promise<Response> {
-    const reqBody = await req.json();
-    const { arguments: { timezone } } = reqBody;
-    const result = await now({ timezone });
-    return NextResponse.json(result);
-}
